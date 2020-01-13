d38e603dcdaa2cba3d74de890176ed7370e6da4a
kcheung3102 <kcheung3102@gmail.com>
2020-01-12 22:26:29 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Dropzone  from 'react-dropzone';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  onDrop = (acceptedFiles) => {
+    console.log(acceptedFiles);
+  }
+  
+  render() {
+    const maxSize = 1048576;
+    return (
+      <div className="text-center mt-5">
+        <Dropzone
+          onDrop={this.onDrop}
+          accept="image/jpeg, image/png"
+          minSize={0}
+          maxSize={maxSize}
+          multiple = {false}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {({getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles}) => {
+            const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+            return (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                {!isDragActive && 'Click here or drop a file to upload!'}
+                {isDragActive && !isDragReject && "Drop it like it's hot!"}
+                {isDragReject && "File type not accepted, sorry!"}
+                {isFileTooLarge && (
+                  <div className="text-danger mt-2">
+                    File is too large.
+                  </div>
+                )}
+              </div>
+            )}
+          }
+        </Dropzone>
+      </div>
+    );
+  }
 }
-
 export default App;
